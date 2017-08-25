1a1e2378d9ceffbe71938e1460c37f60d45c2c93
Craig Meinschein <cmeinsch@gmail.com>
2017-08-24 21:12:57 -0700

    Add table headers to exerciselog page

--- src/ExerciseLog.js
+++ src/ExerciseLog.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Table } from 'semantic-ui-react'
+import './ExerciseLog.css'
 
+// TODO table of all exercises
+// TODO summary/best-record at the top
+// TODO graphs over time?
 const ExerciseLog = () => {
   return (
-    <h1>Exercise Log</h1>
+    <div>
+      <h1>Exercise Log</h1>
+      <Table celled className='exercise-log-table'>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Date</Table.HeaderCell>
+            <Table.HeaderCell>Exercise</Table.HeaderCell>
+            <Table.HeaderCell>Weight</Table.HeaderCell>
+            <Table.HeaderCell>Reps</Table.HeaderCell>
+            <Table.HeaderCell>Sets</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+      </Table>
+    </div>
   )
 }
 export default ExerciseLog
